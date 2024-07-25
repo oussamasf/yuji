07455af221168067bcbba07b6f341da9aed69b67
oussama seffai <ousf.dev@gmail.com>
2024-07-25 21:13:42 +0100

    simple tcp

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+)
 
 func main() {
-	fmt.Println("Hello, Go!")
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Println("Error listening:", err)
+		return
+	}
+	defer listener.Close()
+	fmt.Println("Server is listening on :8080")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+			continue
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	buffer := make([]byte, 1024)
+	_, err := conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Error reading:", err)
+		return
+	}
+
+	fmt.Printf("Received: %s\n", string(buffer))
+
+	_, err = conn.Write(buffer)
+	if err != nil {
+		fmt.Println("Error writing:", err)
+		return
+	}
 }
