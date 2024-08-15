fa6b34ae9c81b7fb36ebe7a16517412f6cb38d62
oussama seffai <ousf.dev@gmail.com>
2024-08-15 12:57:01 +0100

    refactor write resp

--- utils/connectionHandler.go
+++ utils/connectionHandler.go
@@ -2,17 +2,12 @@ package utils
 
 import (
 	"bytes"
-	"fmt"
 	"log"
 	"net"
+	"strconv"
 	"strings"
+	"time"
 )
-
-func writeResponse(conn net.Conn, message string) {
-	if _, err := conn.Write([]byte(message + "\n")); err != nil {
-		log.Printf("Error writing response: %v", err)
-	}
-}
 
 func HandleConnection(conn net.Conn, cache map[string]string, isSlave bool) {
 	infoRes := []string{"role:master", "master_replid:8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb", "master_repl_offset:0"}
@@ -24,41 +19,39 @@ func HandleConnection(conn net.Conn, cache map[string]string, isSlave bool) {
 		n, err := conn.Read(data)
 		if err != nil {
 			log.Printf("Error parsing command: %v", err)
-			writeResponse(conn, "ERROR: Invalid command")
+			WriteResponse(conn, "ERROR: Invalid command")
 			continue
 		}
 
 		trimmedData := bytes.TrimRight(data[:n], "\x00")
-
 		formattedInput := strings.ReplaceAll(string(trimmedData), "\\r\\n", "\r\n")
-
 		commands, err := Parser(formattedInput)
 
 		if err != nil {
 			log.Printf("Error parsing command: %v", err)
-			writeRESPError(conn, "ERROR: Invalid command")
+			WriteRESPError(conn, "ERROR: Invalid command")
 			continue
 		}
 
 		if commands.Type != '*' {
-			writeRESPError(conn, "ERROR: Expected array for command")
+			WriteRESPError(conn, "ERROR: Expected array for command")
 			continue
 		}
 
 		args, ok := commands.Value.([]RESPValue)
 		if !ok {
-			writeRESPError(conn, "ERROR: Invalid command format")
+			WriteRESPError(conn, "ERROR: Invalid command format")
 			continue
 		}
 
 		if len(args) < 1 {
-			writeRESPError(conn, "ERROR: No command given")
+			WriteRESPError(conn, "ERROR: No command given")
 			continue
 		}
 
 		cmdName, ok := args[0].Value.(string)
 		if !ok {
-			writeRESPError(conn, "ERROR: Invalid command name")
+			WriteRESPError(conn, "ERROR: Invalid command name")
 			continue
 		}
 
@@ -67,87 +60,80 @@ func HandleConnection(conn net.Conn, cache map[string]string, isSlave bool) {
 			if isSlave {
 				infoRes = []string{"role:slave"}
 			}
-			writeResponse(conn, NewBulkString(infoRes))
+			WriteResponse(conn, NewBulkString(infoRes))
 
 		case "echo":
 			if len(args) != 2 {
-				writeRESPError(conn, "ERROR: INVALID_NUMBER_OF_ARGUMENTS")
+				WriteRESPError(conn, "ERROR: INVALID_NUMBER_OF_ARGUMENTS")
 				continue
 			}
 			msg, ok := args[1].Value.(string)
 			if !ok {
-				writeRESPError(conn, "ERROR: INVALID_ARGUMENT_TYPE")
+				WriteRESPError(conn, "ERROR: INVALID_ARGUMENT_TYPE")
 				continue
 			}
-			writeRESPBulkString(conn, msg)
+			WriteRESPBulkString(conn, msg)
 
 		case "ping":
 			log.Printf("PONG")
-			writeRESPSimpleString(conn, "PONG")
+			WriteRESPSimpleString(conn, "PONG")
 
-			// case "set":
-			// 	fmt.Println(commands.Args)
-			// 	if len(commands.Args) == 2 {
-			// 		cache[commands.Args[0]] = commands.Args[1]
-			// 		writeResponse(conn, "OK")
-			// 	} else if len(commands.Args) == 4 {
-			// 		if strings.ToLower(commands.Args[2]) == "px" {
-			// 			cache[commands.Args[0]] = commands.Args[1]
-			// 			parsedInt, err := strconv.Atoi(commands.Args[3])
-			// 			if err != nil {
-			// 				writeResponse(conn, "ERROR: INVALID_PX")
-			// 				continue
-			// 			}
-			// 			time.AfterFunc(time.Duration(parsedInt)*time.Millisecond, func() {
-			// 				delete(cache, commands.Args[0])
-			// 			})
-			// 			writeResponse(conn, "OK")
-			// 		} else {
-			// 			writeResponse(conn, "ERROR: INVALID_ARGUMENT")
-			// 			return
-			// 		}
-			// 	} else {
-			// 		writeResponse(conn, "ERROR: INVALID_NUMBER_OF_ARGUMENTS")
-			// 		return
-			// 	}
+		case "set":
+			if len(args) < 3 {
+				WriteRESPError(conn, "ERROR: INVALID_NUMBER_OF_ARGUMENTS")
+				continue
+			}
+			key, ok := args[1].Value.(string)
+			if !ok {
+				WriteRESPError(conn, "ERROR: INVALID_ARGUMENT_TYPE")
+				continue
+			}
+			value, ok := args[2].Value.(string)
+			if !ok {
+				WriteRESPError(conn, "ERROR: INVALID_ARGUMENT_TYPE")
+				continue
+			}
+			cache[key] = value
+			if len(args) == 4 {
+				if strings.ToLower(args[3].Value.(string)) == "px" {
+					expiry, err := strconv.Atoi(args[4].Value.(string))
+					if err != nil {
+						WriteRESPError(conn, "ERROR: INVALID_PX")
+						continue
+					}
+					time.AfterFunc(time.Duration(expiry)*time.Millisecond, func() {
+						delete(cache, key)
+					})
+				} else {
+					WriteRESPError(conn, "ERROR: INVALID_ARGUMENT")
+					continue
+				}
+			}
+			WriteRESPSimpleString(conn, "OK")
 
-			// case "get":
-			// fmt.Println(cache)
-			// if len(commands.Args) > 1 {
-			// 	writeResponse(conn, "ERROR: INVALID_NUMBER_OF_ARGUMENTS")
-			// 	return
-			// }
-			// result := cache[commands.Args[0]]
-			// if result == "" {
-			// 	writeResponse(conn, "NULL")
-			// } else {
-			// 	writeResponse(conn, result)
-			// }
+		case "get":
+			if len(args) != 2 {
+				WriteRESPError(conn, "ERROR: INVALID_NUMBER_OF_ARGUMENTS")
+				continue
+			}
+			key, ok := args[1].Value.(string)
+			if !ok {
+				WriteRESPError(conn, "ERROR: INVALID_ARGUMENT_TYPE")
+				continue
+			}
+			result := cache[key]
+			if result == "" {
+				WriteRESPBulkString(conn, "")
+			} else {
+				WriteRESPBulkString(conn, result)
+			}
 
 		default:
-			writeResponse(conn, "ERROR: Unknown command")
+			WriteResponse(conn, "ERROR: Unknown command")
 			return
 		}
 	}
 }
 
-func writeRESPBulkString(conn net.Conn, message string) {
-	response := fmt.Sprintf("$%d\r\n%s\r\n", len(message), message)
-	if _, err := conn.Write([]byte(response)); err != nil {
-		log.Printf("Error writing response: %v", err)
-	}
-}
-
-func writeRESPSimpleString(conn net.Conn, message string) {
-	response := fmt.Sprintf("+%s\r\n", message)
-	if _, err := conn.Write([]byte(response)); err != nil {
-		log.Printf("Error writing response: %v", err)
-	}
-}
-
-func writeRESPError(conn net.Conn, message string) {
-	response := fmt.Sprintf("-%s\r\n", message)
-	if _, err := conn.Write([]byte(response)); err != nil {
-		log.Printf("Error writing response: %v", err)
-	}
-}
+// SET
+// *3\r\n$8\r\nSET\r\n$14\r\nport\r\n$4\r\n6380\r\n
--- utils/stringHelper.go
+++ utils/stringHelper.go
@@ -2,6 +2,8 @@ package utils
 
 import (
 	"fmt"
+	"log"
+	"net"
 	"strings"
 )
 
@@ -16,4 +18,31 @@ func NewBulkString(arr []string) string {
 	}
 	joinedStr := strings.Join(arr, "\r\n")
 	return fmt.Sprintf("$%d\r\n%v\r\n", len(joinedStr), joinedStr)
+}
+
+func WriteRESPBulkString(conn net.Conn, message string) {
+	response := fmt.Sprintf("$%d\r\n%s\r\n", len(message), message)
+	if _, err := conn.Write([]byte(response)); err != nil {
+		log.Printf("Error writing response: %v", err)
+	}
+}
+
+func WriteRESPSimpleString(conn net.Conn, message string) {
+	response := fmt.Sprintf("+%s\r\n", message)
+	if _, err := conn.Write([]byte(response)); err != nil {
+		log.Printf("Error writing response: %v", err)
+	}
+}
+
+func WriteRESPError(conn net.Conn, message string) {
+	response := fmt.Sprintf("-%s\r\n", message)
+	if _, err := conn.Write([]byte(response)); err != nil {
+		log.Printf("Error writing response: %v", err)
+	}
+}
+
+func WriteResponse(conn net.Conn, message string) {
+	if _, err := conn.Write([]byte(message + "\n")); err != nil {
+		log.Printf("Error writing response: %v", err)
+	}
 }
