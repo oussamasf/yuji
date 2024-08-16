0ac9ea62f22d0af543f983f0741e8c90a0672f18
oussama seffai <ousf.dev@gmail.com>
2024-08-16 15:27:16 +0100

    refactor handle connection

--- main.go
+++ main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"bytes"
 	"flag"
 	"fmt"
-	"log"
 	"net"
 	"strings"
 
@@ -35,7 +33,7 @@ func main() {
 		}
 		isSlave = true
 
-		go SendHandshake(RSlice[0], RSlice[1], *port)
+		go replicaConnection(RSlice[0], RSlice[1], *port)
 	}
 
 	listener, err := net.Listen("tcp", ":"+*port)
@@ -56,34 +54,5 @@ func main() {
 		}
 
 		go utils.HandleConnection(conn, redisMap, isSlave)
-	}
-}
-
-func SendHandshake(masterHost string, masterPort string, replicaPort string) {
-	address := fmt.Sprintf("%s:%s", masterHost, masterPort)
-	m, err := net.Dial("tcp", address)
-	if err != nil {
-		log.Fatalln("couldn't connect to master at ", address)
-	}
-	m.Write([]byte("*1\r\n$4\r\nping\r\n"))
-
-	buffer := make([]byte, 1028)
-
-	n, err := m.Read(buffer)
-	if err != nil {
-		log.Fatalln("couldn't receive the pong")
-	}
-	trimmedBuffer := bytes.Trim(buffer[:n], "\x00\r\n")
-	response := string(trimmedBuffer)
-	fmt.Printf("Response: %q\n", response)
-
-	if strings.ToLower(response) == "+pong" {
-		replConfig := fmt.Sprintf("*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n%s\r\n", replicaPort)
-
-		m.Write([]byte(replConfig))
-		m.Write([]byte("*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n"))
-		m.Write([]byte("*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n"))
-	} else {
-		fmt.Println("Received unexpected response")
 	}
 }
